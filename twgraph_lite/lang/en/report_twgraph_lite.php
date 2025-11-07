3de81ccf22721885580c4e6a6bb7be9cd21a0270
Travis <travis@traviswilhelm.com.au>
2025-11-08 09:49:00 +1030

    Update report_twgraph_lite.php

--- twgraph_lite/lang/en/report_twgraph_lite.php
+++ twgraph_lite/lang/en/report_twgraph_lite.php
@@ -25,3 +25,4 @@
 $string['graph_title'] = '{$a->first} {$a->last}';
 $string['no_data'] = 'No data found';
 $string['pluginname'] = 'TWGraph_Lite';
+$string['privacy:metadata'] = 'The TWGraph_Lite plugin does not store any personal data.';
